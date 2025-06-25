30fefbc3aace5abae7dcf355e2c60d766380bb8f
last-blade <meprashanttyagi2000@gmail.com>
2025-06-25 11:51:16 +0530

    Add access and refresh token options to constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,14 @@
 export const DB_NAME = "machine-maintenance-backend-srpk-prashant";
+export const accessTokenOptions = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+};
+
+export const refreshTokenOptions = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 28 * 24 * 60 * 60 * 1000,
+}
