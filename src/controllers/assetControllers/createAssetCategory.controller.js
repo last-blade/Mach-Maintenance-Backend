1261a3decafcefd93d4c8946f7723590a977849a
last-blade <meprashanttyagi2000@gmail.com>
2025-06-25 15:04:20 +0530

    Add createAssetCategory controller for asset category creation

---
+++ src/controllers/assetControllers/createAssetCategory.controller.js
@@ -0,0 +1,23 @@
+import { apiError, apiResponse, AssetCategory, asyncHandler } from "../allImports.js";
+
+const createAssetCategory = asyncHandler(async (request, response) => {
+    const {assetCategory, depreciationPercentage} = request.body;
+
+    if(!assetCategory && !depreciationPercentage){
+        throw new apiError(404, "All fields are required")
+    }
+
+    const category = await AssetCategory.create({
+        assetCategory,
+        depreciationPercentage,
+        assetCategoryCreator: request.user.id,
+    });
+
+    return response.status(201)
+    .json(
+        new apiResponse(201, category, "Asset category added")
+    )
+
+});
+
+export {createAssetCategory}
