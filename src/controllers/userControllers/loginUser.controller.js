048bd45f1aaa25ea0bb2f96014004710552cf5e2
last-blade <meprashanttyagi2000@gmail.com>
2025-06-25 11:51:30 +0530

    Add loginUser controller for user authentication functionality

---
+++ src/controllers/userControllers/loginUser.controller.js
@@ -0,0 +1,42 @@
+import { accessTokenOptions, refreshTokenOptions } from "../../constants.js";
+import { apiError, apiResponse, asyncHandler, generateAccessToken, generateRefreshToken, User } from "../allImports.js";
+
+const loginUser = asyncHandler(async (request, response) => {
+    const {email, password} = request.body;
+
+    if(!email.trim() && !password){
+        throw new apiError(404, "All fields are required")
+    }
+
+    const foundUser = await User.findOne({email}).select("-password -refreshToken -_id -__v");
+
+    if(!foundUser){
+        throw new apiError(404, "User with this email does not exists")
+    }
+
+    const isValidPassword = await foundUser.isPasswordCorrect(password);
+
+    if(!isValidPassword){
+        throw new apiError(401, "Incorrect password")
+    }
+
+    const accessToken = await generateAccessToken(foundUser._id);
+    const refreshToken = await generateRefreshToken(foundUser._id);
+
+    if(!accessToken && !refreshToken){
+        throw new apiError(500, "Error in generating access and refresh token")
+    }
+
+    foundUser.refreshToken = refreshToken;
+    foundUser.save({validateBeforeSave: false})
+
+    return response.status(200)
+    .cookie("accessToken", accessToken, accessTokenOptions)
+    .cookie("refreshToken", refreshToken, refreshTokenOptions)
+    .json(
+        new apiResponse(200, foundUser, "Logged in successfully")
+    )
+
+});
+
+export {loginUser}
