e9b8cc95bcc16cc195d179fcd470b1b6296b8759
last-blade <meprashanttyagi2000@gmail.com>
2025-06-25 15:51:09 +0530

    Add routes for asset brand management

--- src/routes/asset.routes.js
+++ src/routes/asset.routes.js
@@ -5,26 +5,30 @@ import { createAssetCategory } from "../controllers/assetControllers/assetCatego
 import { fetchAssetCategories } from "../controllers/assetControllers/assetCategoryControllers/fetchAssetCategories.controller.js";
 import { editAssetCategory } from "../controllers/assetControllers/assetCategoryControllers/editAssetCategory.controller.js";
 import { deleteAssetCategory } from "../controllers/assetControllers/assetCategoryControllers/deleteAssetCategory.controller.js";
+import { createAssetBrand } from "../controllers/assetControllers/assetBrandControllers/createAssetBrand.controller.js";
+import { fetchAssetBrands } from "../controllers/assetControllers/assetBrandControllers/fetchAssetBrands.controller.js";
+import { editAssetBrand } from "../controllers/assetControllers/assetBrandControllers/editAssetBrand.controller.js";
+import { deleteAssetBrand } from "../controllers/assetControllers/assetBrandControllers/deleteAssetBrand.controller.js";
 
 const router = Router();
 
 //POST
 router.route("/add-asset").post(authentication, addAsset);
 router.route("/add-asset-category").post(authentication, createAssetCategory);
+router.route("/add-asset-brand").post(authentication, createAssetBrand);
 
 //GET
 router.route("/asset-categories").get(authentication, fetchAssetCategories);
+router.route("/asset-brands").get(authentication, fetchAssetBrands);
 
 //PATCH
 
 //PUT
-router
-  .route("/edit-asset-category/:assetCategoryId")
-  .put(authentication, editAssetCategory);
+router.route("/edit-asset-category/:assetCategoryId").put(authentication, editAssetCategory);
+router.route("/edit-asset-brand/:assetBrandId").put(authentication, editAssetBrand);
 
 //DELETE
-router
-  .route("/delete-asset-category/:assetCategoryId")
-  .delete(authentication, deleteAssetCategory);
+router.route("/delete-asset-category/:assetCategoryId").delete(authentication, deleteAssetCategory);
+router.route("/delete-asset-brand/:assetBrandId").delete(authentication, deleteAssetBrand);
 
 export default router;
