f6e4ae3ad94a5dd44f90eedcb865aa934f610d2f
last-blade <meprashanttyagi2000@gmail.com>
2025-06-25 15:04:11 +0530

    Add addAsset controller to handle asset creation and QR code generation

---
+++ src/controllers/assetControllers/addAsset.controller.js
@@ -0,0 +1,42 @@
+import { apiError, apiResponse, Asset, asyncHandler } from "../allImports.js";
+import QRCode from "qrcode";
+
+const addAsset = asyncHandler(async (request, response) => {
+    const {assetCategory, assetModelNo, assetName, assetCode, 
+        assetBrand, barcodeNo, invoiceNo, purchaseDate, 
+        price, specialAsset, assetStatus, supplier, assetLocation
+    } = request.body;
+
+    if([assetCategory, assetModelNo, assetName, assetCode, 
+        assetBrand, purchaseDate, price, specialAsset, 
+        assetStatus, supplier, assetLocation].some(inputField => inputField === undefined || inputField.trim() === "")
+    ){
+        throw new apiError(404, "All mandatory fields are required to fill")
+    }
+
+    const newASSET = await Asset.create({
+        assetCategory, assetModelNo, assetName, assetCode, assetBrand, 
+        barcodeNo: barcodeNo || "", 
+        invoiceNo: invoiceNo || "", 
+        purchaseDate, price, specialAsset, assetStatus, supplier, assetLocation,
+        assetCreator: request.user.id,
+    });
+
+    const qrData = `Asset ID: ${newASSET._id}
+                    Asset Code: ${newASSET.assetCode}
+                    Asset Name: ${newASSET.assetName}
+                    Brand: ${newASSET.assetBrand} | ID: ${newASSET._id}`
+    ;
+    const qrCodeDataUri = await QRCode.toDataURL(qrData); 
+
+    return response.status(201)
+    .json(
+        new apiResponse(201, {
+            asset: newASSET, 
+            qrCode: qrCodeDataUri
+        }, "Asset added")
+    )
+
+});
+
+export {addAsset}
