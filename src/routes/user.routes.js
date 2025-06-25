3bf47c13faf00a202486574504fa420865c3c50b
last-blade <meprashanttyagi2000@gmail.com>
2025-06-25 12:09:39 +0530

    Add login route to user routes for handling user login requests

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/userControllers/registerUser.controller.js";
+import { loginUser } from "../controllers/userControllers/loginUser.controller.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
 
 export default router
