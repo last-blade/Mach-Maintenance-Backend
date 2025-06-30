f43d144d9097b84a9ed56f031ae2b7645044f39a
last-blade <meprashanttyagi2000@gmail.com>
2025-06-30 12:33:28 +0530

    Add deleteDepartment controller to remove existing departments with validation

---
+++ src/controllers/userControllers/hrManagementControllers/deleteDepartment.controller.js
@@ -0,0 +1,18 @@
+import { apiError, apiResponse, asyncHandler, Department } from "../../allImports";
+
+const deleteDepartment = asyncHandler(async (request, response) => {
+    const {departmentId} = request.params;
+
+    if(!departmentId){
+        throw new apiError(400, "Department ID is required")
+    }
+
+    await Department.findByIdAndDelete(departmentId);
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, {}, "Department deleted")
+    )
+});
+
+export {deleteDepartment}
