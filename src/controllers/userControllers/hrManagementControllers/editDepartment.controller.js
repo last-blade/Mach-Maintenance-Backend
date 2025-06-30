302ef8e4245585e95f360cc8d35cf81764acc604
last-blade <meprashanttyagi2000@gmail.com>
2025-06-30 12:33:23 +0530

    Add editDepartment controller to update existing departments with validation

---
+++ src/controllers/userControllers/hrManagementControllers/editDepartment.controller.js
@@ -0,0 +1,37 @@
+import { apiError, apiResponse, asyncHandler, Department } from "../../allImports.js";
+
+const editDepartment = asyncHandler(async (request, response) => {
+    const {departmentId} = request.params;
+    const {department} = request.body;
+
+    if(!departmentId){
+        throw new apiError(400, "Department ID is required")
+    }
+
+    const foundDepartment = await Department.findById(departmentId);
+
+    if(!foundDepartment){
+        throw new apiError(500, "Department not found maybe deleted")
+    }
+
+    if(!department){
+        throw new apiError(400, "Department is required")
+    }
+
+    // foundDepartment.department = department;
+    // foundDepartment.save({validateBeforeSave: false});
+
+    const updatedDepartment = await Department.findByIdAndUpdate(departmentId, {
+        $set: {
+            department
+        }
+    }, {new: true}).select("departmentCreator");
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, updatedDepartment, "Department updated")
+    )
+
+});
+
+export {editDepartment}
