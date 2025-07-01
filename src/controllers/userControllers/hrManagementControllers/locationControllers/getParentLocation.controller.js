b929a303ab203673105998c3a8e5ca5d46341457
last-blade <meprashanttyagi2000@gmail.com>
2025-07-01 21:15:52 +0530

    Add getParentLocation controller to fetch parent locations

---
+++ src/controllers/userControllers/hrManagementControllers/locationControllers/getParentLocation.controller.js
@@ -0,0 +1,15 @@
+import { apiResponse, asyncHandler, Location } from "../../../allImports.js";
+
+const getParentLocation = asyncHandler(async (request, response) => {
+    const location = await Location.find({
+        locationCreator: request.user.id,
+        isFactory: true,
+    }).select("-locationCreator -__v");
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, location, "Parent location fetched")
+    )
+});
+
+export {getParentLocation}
