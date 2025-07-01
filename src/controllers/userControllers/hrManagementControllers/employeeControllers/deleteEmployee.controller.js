74e53430fec68e63b865264badf44901109d8c92
last-blade <meprashanttyagi2000@gmail.com>
2025-07-01 17:20:02 +0530

    Add deleteEmployee controller for employee removal functionality

---
+++ src/controllers/userControllers/hrManagementControllers/employeeControllers/deleteEmployee.controller.js
@@ -0,0 +1,21 @@
+import { apiError, apiResponse, asyncHandler, Employee } from "../../../allImports";
+
+const deleteEmployee = asyncHandler(async (request, response) => {
+    const {employeeId} = request.params;
+
+    const foundEmployee = await Employee.findById(employeeId);
+
+    if(!foundEmployee){
+        throw new apiError(404, "Emplyee not found maybe deleted")
+    }
+
+    await Employee.findByIdAndDelete(employeeId);
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, {}, "Employee deleted")
+    )
+
+});
+
+export {deleteEmployee}
