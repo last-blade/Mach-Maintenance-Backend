64a98526155908847a061342724ca19f5370ede6
last-blade <meprashanttyagi2000@gmail.com>
2025-07-01 21:27:50 +0530

    Add PUT route for editing location details with authentication

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,13 +5,14 @@ import { authentication } from "../middlewares/auth.middleware.js";
 import { addChildLocation, addLocation, addParentLocation } from "../controllers/userControllers/hrManagementControllers/locationControllers/addLocation.controller.js";
 import { getLocation } from "../controllers/userControllers/hrManagementControllers/locationControllers/getLocation.controller.js";
 import { getParentLocation } from "../controllers/userControllers/hrManagementControllers/locationControllers/getParentLocation.controller.js";
+import { editLocation } from "../controllers/userControllers/hrManagementControllers/locationControllers/editLocation.controller.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
-
+//~LOCATION-ROUTES
 //Location Routes - POST
 router.route("/hr/location/add-location").post(authentication, addLocation)
 router.route("/hr/location/add-child-location").post(authentication, addChildLocation);
@@ -19,5 +20,7 @@ router.route("/hr/location/add-parent-location").post(authentication, addParentL
 //Location Routes - GET
 router.route("/hr/location/get-location/:locationId").get(authentication, getLocation);
 router.route("/hr/location/get-parent-location").get(authentication, getParentLocation);
+//Location Routes - PUT
+router.route("/hr/edit-location/:locationId").put(authentication, editLocation);
 
 export default router
