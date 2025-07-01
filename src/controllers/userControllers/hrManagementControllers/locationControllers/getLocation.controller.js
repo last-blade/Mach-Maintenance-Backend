ae405f7ba4ca01d1a632adb45c23888e420994fa
last-blade <meprashanttyagi2000@gmail.com>
2025-07-01 18:03:15 +0530

    Add getLocation controller to fetch location by ID for user reference

---
+++ src/controllers/userControllers/hrManagementControllers/locationControllers/getLocation.controller.js
@@ -0,0 +1,19 @@
+import { apiResponse, asyncHandler, Location } from "../../../allImports.js";
+
+const getLocation = asyncHandler(async (request, response) => {
+
+    const {locationId} = request.params;
+
+    const location = await Location.find({
+        locationCreator: request.user.id,
+        _id: locationId,
+        // isFactory: true,
+    }).select("-locationCreator");
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, location, "Location fetched")
+    )
+});
+
+export {getLocation}
