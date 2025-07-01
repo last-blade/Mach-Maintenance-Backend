6cb340d68be7b585336b29fa832d253bece4269d
last-blade <meprashanttyagi2000@gmail.com>
2025-07-01 16:55:15 +0530

    Enhance location management by adding isFactory parameter to addLocation and refining addChildLocation logic

--- src/controllers/userControllers/hrManagementControllers/addLocation.controller.js
+++ src/controllers/userControllers/hrManagementControllers/addLocation.controller.js
@@ -1,7 +1,7 @@
 import { apiError, apiResponse, asyncHandler, Location } from "../../allImports.js";
 
 const addLocation = asyncHandler(async (request, response) => {
-    const {locationName, locationCode} = request.body;
+    const {locationName, locationCode, isFactory} = request.body;
 
     if(!locationName.trim() || !locationCode.trim()){
         throw new apiError(400, "All fields are required")
@@ -10,6 +10,7 @@ const addLocation = asyncHandler(async (request, response) => {
     const location = await Location.create({
         locationName, 
         locationCode: locationCode.toUpperCase(),
+        isFactory: isFactory || false,
     });
 
     return response.status(201)
@@ -20,38 +21,132 @@ const addLocation = asyncHandler(async (request, response) => {
 });
 
 const addChildLocation = asyncHandler(async (request, response) => {
-    const {locationName, locationCode, locationParentId, locationChildId} = request.body;
+  const { locationName, locationCode, locationId } = request.body;
 
-    if(!locationName.trim() || !locationCode.trim()){
-        throw new apiError(400, "All fields are required")
+  if (!locationId) {
+    throw new apiError(400, "Parent location ID is required")
+  }
+
+  const parentLocation = await Location.findById(locationId);
+
+  // agar parent location nahin hai iss location id kaa, toh fir yeh khud parent hai yaa factory hai
+  if (parentLocation?.hasParent === false) {
+    const location = await Location.create({
+      locationName,
+      locationCode: locationCode.toUpperCase(),
+      locationParentId: locationId,
+    });
+
+    await Location.findByIdAndUpdate(locationId, {
+      $push: { children: location._id },
+    }, {new: true});
+
+    location.hasParent = true;
+    await location.save({ validateBeforeSave: false });
+
+    const updatedLocation = await Location.findById(locationId);
+
+    return response.status(201)
+    .json(
+        new apiResponse(201, updatedLocation, "Child location added")
+    );
+  } 
+  
+  else if(parentLocation?.hasParent === true || parentLocation?.isFactory === true){
+    const location = await Location.create({
+      locationName,
+      locationCode: locationCode.toUpperCase(),
+    });
+
+    const updatedLocation = await Location.findByIdAndUpdate(locationId, {
+        $push: {
+            children: location._id,
+        }
+    }, {new: true})
+
+    return response.status(201)
+    .json(
+        new apiResponse(201, updatedLocation, "Child location added")
+    )
+
+  }
+});
+
+const addParentLocation = asyncHandler(async (request, response) => {
+    const { locationName, locationCode, locationId } = request.body;
+
+    if (!locationId) {
+        throw new apiError(400, "Parent location ID is required")
     }
 
-    if(locationParentId){
-        const childLocation = await Location.create({
+  const childLocation = await Location.findById(locationId);
+
+  if(childLocation?.hasParent === true){
+    const location = await Location.create({
+        locationName,
+        locationCode,
+        locationParentId: childLocation.locationParentId,
+    });
+
+    const oldPrentLocationId = childLocation.locationParentId;
+
+    childLocation.locationParentId = location._id;
+    await childLocation.save({validateBeforeSave: false});
+
+    location.hasParent = true;
+    await location.save({validateBeforeSave: false});
+    // location.locationParentId = oldPrentLocationId;
+    // await location.save({validateBeforeSave: false});
+
+    await Location.findByIdAndUpdate(oldPrentLocationId, {
+        $pull: {
+            children: locationId,
+        }
+    }, {new: true});
+
+    await Location.findByIdAndUpdate(oldPrentLocationId, {
+        $push: {
+            children: location._id,
+        }
+    }, {new: true});
+
+    const updatedLocation = await Location.findById(oldPrentLocationId);
+
+    return response.status(201)
+    .json(
+        new apiResponse(201, updatedLocation, "Parent location added")
+    )
+
+  }
+
+    else if(childLocation?.hasParent === false){
+        const location = await Location.create({
             locationName,
             locationCode,
-            hasParent: true,
         });
 
-        const foundChildLocation = await Location.findById(childLocation._id);
+        await Location.findByIdAndUpdate(location?._id, {
+            $push: {
+                children: locationId,
+            }
+        }, {new: true});
 
-        foundChildLocation.children = childLocation._id;
-        foundChildLocation.save({validateBeforeSave: false})
-    }
+        childLocation.locationParentId = location?._id;
+        childLocation.hasParent = true;
+        await childLocation.save({ validateBeforeSave: false });
 
-    else if(locationChildId){
-        const parentLocation = await Location.create({
-            locationName,
-            locationCode,
-            hasParent: true,
-        });
 
-        const foundParentLocation = await Location(parentLocation._id);
+        const updatedLocation = await Location.findById(location._id)
 
-        // foundParentLocation.children = 
-
+        return response.status(201)
+        .json(
+            new apiResponse(201, updatedLocation, "Parent location added")
+        )
     }
 
 });
+
+export {addParentLocation}
+
 
 export{addLocation, addChildLocation}
