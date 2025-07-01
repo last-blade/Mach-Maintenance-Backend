e91f90c027b01d92471330296886eb6b0c0169eb
last-blade <meprashanttyagi2000@gmail.com>
2025-07-01 21:08:25 +0530

    Add HR location management routes with authentication

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/userControllers/registerUser.controller.js";
 import { loginUser } from "../controllers/userControllers/loginUser.controller.js";
+import { authentication } from "../middlewares/auth.middleware.js";
+import { addChildLocation, addLocation, addParentLocation } from "../controllers/userControllers/hrManagementControllers/locationControllers/addLocation.controller.js";
+import { getLocation } from "../controllers/userControllers/hrManagementControllers/locationControllers/getLocation.controller.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+
+
+//Location Routes - POST
+router.route("/hr/location/add-location").post(authentication, addLocation)
+router.route("/hr/location/add-child-location").post(authentication, addChildLocation);
+router.route("/hr/location/add-parent-location").post(authentication, addParentLocation);
+//Location Routes - GET
+router.route("/hr/location/get-location/:locationId").get(authentication, getLocation);
 
 export default router
