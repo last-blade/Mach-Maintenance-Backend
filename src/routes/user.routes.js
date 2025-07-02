070b5b75976ba171fc839831c71e3dfe2619ee3f
last-blade <meprashanttyagi2000@gmail.com>
2025-07-02 16:20:52 +0530

    Add department routes to user.routes.js for managing departments

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,6 +7,10 @@ import { getLocation } from "../controllers/userControllers/hrManagementControll
 import { getParentLocation } from "../controllers/userControllers/hrManagementControllers/locationControllers/getParentLocation.controller.js";
 import { editLocation } from "../controllers/userControllers/hrManagementControllers/locationControllers/editLocation.controller.js";
 import { deleteLocation } from "../controllers/userControllers/hrManagementControllers/locationControllers/deleteLocation.controller.js";
+import { addDepartment } from "../controllers/userControllers/hrManagementControllers/departmentControllers/addDepartment.controller.js";
+import { fetchDepartment } from "../controllers/userControllers/hrManagementControllers/departmentControllers/fetchDepartment.controller.js";
+import { editDepartment } from "../controllers/userControllers/hrManagementControllers/departmentControllers/editDepartment.controller.js";
+import { deleteDepartment } from "../controllers/userControllers/hrManagementControllers/departmentControllers/deleteDepartment.controller.js";
 
 const router = Router();
 
@@ -25,5 +29,16 @@ router.route("/hr/location/get-parent-location").get(authentication, getParentLo
 router.route("/hr/edit-location/:locationId").put(authentication, editLocation);
 //Location Routes - DELETE
 router.route("/hr/location/delete-location/:locationId").delete(authentication, deleteLocation);
+
+
+//~DEPARTMENT-ROUTES
+//POST
+router.route("/hr/department/add-department").post(authentication, addDepartment);
+//GET
+router.route("/hr/department/departments").get(authentication, fetchDepartment);
+//PUT
+router.route("/hr/department/edit/:departmentId").get(authentication, editDepartment);
+//DELETE
+router.route("/hr/department/delete/:departmentId").get(authentication, deleteDepartment);
 
 export default router
