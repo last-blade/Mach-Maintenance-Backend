ceaadf6898b634c3536caee8b81a13da6a8ca886
last-blade <meprashanttyagi2000@gmail.com>
2025-07-02 15:40:37 +0530

    Add assetMaintenance model with schema and pre-save hook

---
+++ src/models/assetMaintenance.model.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const assetMaintenanceSchema = new Schema({
+
+    maintenanceId: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+
+    remarks: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    mechanic: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+    assetId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Asset",
+        required: true,
+    }
+
+}, {timestamps: true});
+
+assetMaintenanceSchema.pre("save", async function(next){
+    if(this.maintenanceId){
+        this.maintenanceId = "MNTC" + this._id
+    }
+
+    next();
+})
+
+export const AssetMaintenance = mongoose.model("AssetMaintenance", assetMaintenanceSchema);
