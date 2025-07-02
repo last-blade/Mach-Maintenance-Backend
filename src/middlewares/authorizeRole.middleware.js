6986a01453e4d745e5e7c265a18c68d4d0b8c91f
last-blade <meprashanttyagi2000@gmail.com>
2025-07-02 16:59:19 +0530

    Add authorizeRoles middleware for role-based access control

---
+++ src/middlewares/authorizeRole.middleware.js
@@ -0,0 +1,21 @@
+import { asyncHandler } from "../utils/asyncHandler,js";
+
+const authorizeRoles = (...allowedRoles) => {
+    return asyncHandler(async (request, response, next) => {
+        const userRole = request.user?.accountType;
+
+        if(userRole !== allowedRoles.includes(userRole)){
+            return response.status(403)
+            .json({
+                "statusCode": 403,
+                "success": false,
+                "data": null,
+                "errors": ["Access denied"]
+            })
+        }
+
+        next();
+    })
+};
+
+export {authorizeRoles}
