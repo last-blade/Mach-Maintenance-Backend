f8d7a6dbec1228d7e279d3bcdb7657aaf9a4f55d
last-blade <meprashanttyagi2000@gmail.com>
2025-07-14 13:29:10 +0530

    Add authentication middleware to logout route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -21,7 +21,7 @@ const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/logout").post(logoutUser);
+router.route("/logout").post(authentication, logoutUser);
 
 
 //---------------------------------------------------------------HR Routes------------------------------------------------------------------
