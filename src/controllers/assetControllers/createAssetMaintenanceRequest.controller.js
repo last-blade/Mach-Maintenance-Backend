842aa030883040dc2c33ef70a34103c83173edf7
last-blade <meprashanttyagi2000@gmail.com>
2025-07-25 15:35:37 +0530

    fixed typos and added fields while creating request

--- src/controllers/assetControllers/createAssetMaintenanceRequest.controller.js
+++ src/controllers/assetControllers/createAssetMaintenanceRequest.controller.js
@@ -3,6 +3,7 @@ import { apiError, apiResponse, Asset, AssetMaintenanceRequest, asyncHandler } f
 const createAssetMaintenanceRequest = asyncHandler(async (request, response) => {
     const {assetId} = request.params;
     const {remark} = request.body;
+    console.log(request.user)
 
     if(!assetId){
         throw new apiError(400, "Asset ID is required")
@@ -12,7 +13,7 @@ const createAssetMaintenanceRequest = asyncHandler(async (request, response) => 
         throw new apiError(400, "Remark is required")
     }
 
-    const foundAsset = await Asset(assetId);
+    const foundAsset = await Asset.findById(assetId);
 
     if(!foundAsset){
         throw new apiError(404, "Asset does not exist")
@@ -27,7 +28,10 @@ const createAssetMaintenanceRequest = asyncHandler(async (request, response) => 
     const assetMaintenanceRequest = await AssetMaintenanceRequest.create({
         remark,
         assetId,
-    })
+        assetMaintenanceRequestCreator: request.user.id,
+        creatorModel: request.user.accountType === "Admin" || "HR" ? "User" : "Employee",
+    });
+    
 
     return response.status(200).json(
         new apiResponse(200, assetMaintenanceRequest, "Asset maintenance request created")
