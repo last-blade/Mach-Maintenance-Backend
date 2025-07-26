fa840bdb3b9a8569335e1793c4752362504d5e08
last-blade <meprashanttyagi2000@gmail.com>
2025-07-26 18:17:13 +0530

    NA

---
+++ src/controllers/assetControllers/assetMaintenanceControllers/getUnderMaintenanceAssets.controller.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { apiResponse, Asset, AssetMaintenance, asyncHandler } from "../../allImports.js";
+
+const getUnderMaintenanceAssets = asyncHandler(async (request, response) => {
+
+    const underMaintenanceAssets = await Asset.aggregate([
+        // {
+        //     $match: {
+        //         mechanic: new mongoose.Types.ObjectId(request.user.id)
+        //     }
+        // },
+
+        {
+            $lookup: {
+                from: "assetmaintenances",
+                localField: "_id",
+                foreignField: "assetId",
+                as: "assetsUnderMaintenance"
+            }
+        }
+
+    ]); 
+    
+    return response.status(200)
+    .json(
+        new apiResponse(200, underMaintenanceAssets, "fetched")
+    )
+
+});
+
+export {getUnderMaintenanceAssets}
