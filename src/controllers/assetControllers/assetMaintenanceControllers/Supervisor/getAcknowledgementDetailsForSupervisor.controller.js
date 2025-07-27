1a6465cb310fbfd235bebc2b6e24714b1216af18
last-blade <meprashanttyagi2000@gmail.com>
2025-07-27 17:54:17 +0530

    add getAcknowledgementDetailsForSupervisor controller

---
+++ src/controllers/assetControllers/assetMaintenanceControllers/Supervisor/getAcknowledgementDetailsForSupervisor.controller.js
@@ -0,0 +1,10 @@
+import { asyncHandler } from "../../../allImports.js";
+
+const getAcknowledgementDetailsForSupervisor = asyncHandler(async (request, response) => {
+    const {assetId} = request.params;
+
+    
+
+});
+
+export {getAcknowledgementDetailsForSupervisor}
