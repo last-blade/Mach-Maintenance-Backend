e4e8c29cf2a1f9fae5fbcdaaf4fd6301af0d6c71
last-blade <meprashanttyagi2000@gmail.com>
2025-07-28 15:12:29 +0530

    Add accountype

--- src/utils/generateRefreshToken.js
+++ src/utils/generateRefreshToken.js
@@ -1,6 +1,13 @@
+import { Employee } from "../models/employee.model.js";
 import { User } from "../models/user.model.js"
 
-const generateRefreshToken = async (userId) => {
+const generateRefreshToken = async (userId, accountType) => {
+
+    if(accountType === "Supervisor" || accountType === "Mechanic" || accountType === "Production"){
+        const foundUser = await Employee.findById(userId);
+        const refreshToken = foundUser.generateRefreshToken();
+        return refreshToken;
+    }
     const foundUser = await User.findById(userId);
 
     const refreshToken = await foundUser.generateRefreshToken();
