f8d6054a6adf0be0f3cd7adf26b43ed55512a0bb
last-blade <meprashanttyagi2000@gmail.com>
2025-07-28 14:01:14 +0530

    add register route

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { loginEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/loginEmployee.controller.js";
 import { logoutEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/logoutEmployee.controller.js";
+import { addEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/addEmployee.controller.js";
 
 const router = Router();
 
+router.route("/register").post(addEmployee);
 router.route("/login").post(loginEmployee);
 router.route("/logout").post(logoutEmployee);
 
