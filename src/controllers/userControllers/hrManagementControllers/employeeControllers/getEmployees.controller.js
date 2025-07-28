46555bd2c0a3bf315b7d43dc4f5dfc45d948d00c
last-blade <meprashanttyagi2000@gmail.com>
2025-07-28 15:53:26 +0530

    populate department field and hide the passowrd refreshtoken employeecreator

--- src/controllers/userControllers/hrManagementControllers/employeeControllers/getEmployees.controller.js
+++ src/controllers/userControllers/hrManagementControllers/employeeControllers/getEmployees.controller.js
@@ -3,7 +3,7 @@ import { apiResponse, asyncHandler, Employee } from "../../../allImports.js";
 const getEmployees = asyncHandler(async (request, response) => {
   const employees = await Employee.find({
     employeeCreator: request.user.id,
-  });
+  }).populate("department", "department").select("-employeeCreator -refreshToken -password");
 
   return response
     .status(200)
