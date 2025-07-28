dc3efd1ac347ae49fe735469ca181f86acfe67fe
last-blade <meprashanttyagi2000@gmail.com>
2025-07-28 14:02:02 +0530

    add middleware

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { loginEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/loginEmployee.controller.js";
 import { logoutEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/logoutEmployee.controller.js";
 import { addEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/addEmployee.controller.js";
+import { authentication } from "../middlewares/auth.employee.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(addEmployee);
 router.route("/login").post(loginEmployee);
-router.route("/logout").post(logoutEmployee);
+router.route("/logout").post(authentication, logoutEmployee);
 
 export default router;
