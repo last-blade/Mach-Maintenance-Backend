216cf5fb34cb79cf399733810c566acb90a6ee41
last-blade <meprashanttyagi2000@gmail.com>
2025-07-28 15:46:58 +0530

    removed register route

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -2,12 +2,12 @@ import { Router } from "express";
 import { loginEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/loginEmployee.controller.js";
 import { logoutEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/logoutEmployee.controller.js";
 import { addEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/addEmployee.controller.js";
-import { authentication } from "../middlewares/auth.employee.middleware.js";
+import employeeAuth from "../middlewares/auth.employee.middleware.js";
+import { authentication } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(addEmployee);
 router.route("/login").post(loginEmployee);
-router.route("/logout").post(authentication, logoutEmployee);
+router.route("/logout").post(employeeAuth, logoutEmployee);
 
 export default router;
