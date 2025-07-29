3db18d3898e37e3904aee5b3ba18a5bd9476b351
last-blade <meprashanttyagi2000@gmail.com>
2025-07-29 15:09:41 +0530

    changed mechanic refrenece model to employee

--- src/models/assetMaintenance.model.js
+++ src/models/assetMaintenance.model.js
@@ -12,7 +12,7 @@ const assetMaintenanceSchema = new Schema({
 
     mechanic: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+        ref: "Employee",
         required: true,
     },
 
