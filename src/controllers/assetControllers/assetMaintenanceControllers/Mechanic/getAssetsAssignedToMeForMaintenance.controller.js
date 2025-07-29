f71060ac128f7765c0d40350c3c0c57695baa85a
last-blade <meprashanttyagi2000@gmail.com>
2025-07-29 17:46:28 +0530

    changed controller name to getassetsAssignedToMeForMaintenance

---
+++ src/controllers/assetControllers/assetMaintenanceControllers/Mechanic/getAssetsAssignedToMeForMaintenance.controller.js
@@ -0,0 +1,21 @@
+import { apiResponse, Asset, AssetMaintenance, asyncHandler } from "../../../allImports.js";
+
+const getassetsAssignedToMeForMaintenance = asyncHandler(async (request, response) => {
+    const assetsAssignedToMeForMaintenance = await AssetMaintenance.find({
+        mechanic: request.user.id,
+    }).populate("assetId");
+
+    if(assetsAssignedToMeForMaintenance.length === 0){
+        return response.status(200)
+        .json(
+            new apiResponse(200, {}, "No assets found assigned to you for maintenance")
+        )
+    }
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, assetsAssignedToMeForMaintenance, "Assets fetched assigned to you for maintenance")
+    )
+});
+
+export {getassetsAssignedToMeForMaintenance}
