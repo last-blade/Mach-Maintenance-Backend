0c44019896bd569a374e2afe2371af244828a1ca
last-blade <meprashanttyagi2000@gmail.com>
2025-07-29 15:09:19 +0530

    add getAllAssetsWithMechanicAssigned controller, Correct mechanic population and confirm 1:1 mapping with maintenance requests
    - Verified that each AssetMaintenance record has a valid corresponding AssetMaintenanceRequest
    - Ensured mechanic details are fetched directly from AssetMaintenance (not from nested request)
    - Updated aggregation logic explanation to reflect correct mechanic association

---
+++ src/controllers/assetControllers/assetMaintenanceControllers/Supervisor/getAllAssetsWithMechanicAssigned.controller.js
@@ -0,0 +1,62 @@
+import { apiResponse, AssetMaintenance, asyncHandler } from "../../../allImports.js";
+
+const getAllAssetsWithMechanicAssigned = asyncHandler(async (request, response) => {
+    // const mechanic
+
+    const result = await AssetMaintenance.aggregate([
+        {
+            $lookup: {
+                from: "assetmaintenancerequests",
+                let: {requestId: "$assetMaintenanceRequestId"},
+
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: {
+                                $eq: ["$_id", "$$requestId"]
+                            }
+                        }
+                    },
+
+                    {
+                        $lookup: {
+                            from: "assets",
+                            let: {assetID: "$assetId"},
+
+                            pipeline: [
+                                {
+                                    $match: {
+                                        $expr: {
+                                            $eq: ["$_id", "$$assetID"]
+                                        }
+                                    }
+                                }
+                            ],
+
+                            as: "assetDetails"
+                        }
+                    }
+                ],
+                as: "result"
+            },
+        },
+
+          {
+            $lookup: {
+                from: "employees",
+                localField: "mechanic",
+                foreignField: "_id",
+                as: "mechanicDetails"
+            }
+        },
+    ]);
+
+    // const result = await AssetMaintenance.find({}).populate("assetMaintenanceRequestId").populate("mechanic").select("-password -refreshToken -__v")
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, result, "Fetched")
+    )
+});
+
+export {getAllAssetsWithMechanicAssigned}
