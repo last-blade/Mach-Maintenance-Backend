e560f58b0da37870f346d0a404a18c766ca3d28a
last-blade <meprashanttyagi2000@gmail.com>
2025-07-29 17:20:46 +0530

    add getAcknowledgementDetailsForSupervisor to fetch acknowledement

--- src/controllers/assetControllers/assetMaintenanceControllers/Supervisor/getAcknowledgementDetailsForSupervisor.controller.js
+++ src/controllers/assetControllers/assetMaintenanceControllers/Supervisor/getAcknowledgementDetailsForSupervisor.controller.js
@@ -1,9 +1,24 @@
-import { asyncHandler } from "../../../allImports.js";
+/*
+Acknowledgement id humein assets-with-mechanics wali api se jo fetch ho raha hai response uske response mein se send karega frontend user
+or iss assets-with-mechanics api ko frontend wala "acknowledgement" wale Tab mein bhi integrate karega mobile webApp mein.
+or iss api mein acknowledgementid milegi or issi id ko pramas mein send karega user
+*/
+
+import { apiError, apiResponse, asyncHandler, MaintenanceAcknowledgment } from "../../../allImports.js";
 
 const getAcknowledgementDetailsForSupervisor = asyncHandler(async (request, response) => {
-    const {assetId} = request.params;
+    const {acknowledgementId} = request.params;
 
-    
+    const acknowledgement = await MaintenanceAcknowledgment.findById(acknowledgementId);
+
+    if(!acknowledgement){
+        throw new apiError(404, "Acknowledgement not found")
+    }
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, acknowledgement, "Acknowledgement fetched")
+    )
 
 });
 
