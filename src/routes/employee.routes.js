e905c230f5f6905fff509ad9dd1b2a9ef212e901
last-blade <meprashanttyagi2000@gmail.com>
2025-07-29 18:00:06 +0530

    remove unnecessary imports

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import { loginEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/loginEmployee.controller.js";
 import { logoutEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/logoutEmployee.controller.js";
-import { addEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/addEmployee.controller.js";
-import employeeAuth from "../middlewares/auth.employee.middleware.js";
 import { authentication } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/login").post(loginEmployee);
-router.route("/logout").post(employeeAuth, logoutEmployee);
+router.route("/logout").post(authentication, logoutEmployee);
 
 export default router;
