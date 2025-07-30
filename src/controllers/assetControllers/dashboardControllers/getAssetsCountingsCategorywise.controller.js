aca91b0e7d2232f825a7283188ff31b6c609a1b3
last-blade <meprashanttyagi2000@gmail.com>
2025-07-30 14:17:59 +0530

    add getAssetsCountingsCategorywise controller to fetch total assets category and undermaintenance waise

---
+++ src/controllers/assetControllers/dashboardControllers/getAssetsCountingsCategorywise.controller.js
@@ -0,0 +1,40 @@
+import { apiResponse, Asset, asyncHandler } from "../../allImports.js";
+
+const getAssetsCountingsCategorywise = asyncHandler(async (request, response) => {
+    const assetsCountingsForEachCategory = await Asset.aggregate([
+        {
+            $facet: {
+                totalAssets: [
+                    {$count: "count"}
+                ],
+
+                byStatus: [
+                    {
+                        $group: {
+                            _id: "$assetStatus",
+                            total: {$sum: 1}
+                        }
+                    }
+                ],
+
+                underMaintenance: [
+                    {
+                        $group: {
+                            _id: "$underMaintenance",
+                            total: {$sum: 1}
+                        }
+                    }
+                ]
+
+            }
+        }
+
+    ]);
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, assetsCountingsForEachCategory, "Assets countings fetched")
+    )
+});
+
+export {getAssetsCountingsCategorywise}
