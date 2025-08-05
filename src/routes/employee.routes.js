b8919392dda071b1003b42806a550d46f2200483
last-blade <meprashanttyagi2000@gmail.com>
2025-08-05 13:57:48 +0530

    add /employees route to fetch all employees

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { loginEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/loginEmployee.controller.js";
 import { logoutEmployee } from "../controllers/userControllers/hrManagementControllers/employeeControllers/logoutEmployee.controller.js";
 import { authentication } from "../middlewares/auth.middleware.js";
+import { getEmployees } from "../controllers/userControllers/hrManagementControllers/employeeControllers/getEmployees.controller.js";
 
 const router = Router();
 
 router.route("/login").post(loginEmployee);
 router.route("/logout").post(authentication, logoutEmployee);
+router.route("/employees").get(authentication, getEmployees);
 
 export default router;
