8febae91daa1319183f8254104093b2a94330560
last-blade <meprashanttyagi2000@gmail.com>
2025-08-05 11:52:27 +0530

    add assetScheduledMaintenanceSchema to schedule asset's maintenance

---
+++ src/models/assetScheduledMaintenance.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+
+const assetScheduledMaintenanceSchema = new Schema({
+    assetId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Asset",
+        required: true,
+    },
+
+    maintenanceScheduledDate: {
+        type: Date,
+        required: true,
+    },
+
+    maintenanceScheduledId: {
+        type: String,
+        required: true,
+    },
+
+    maintenanceScheduledBy: {
+       type: mongoose.Schema.Types.ObjectId,
+       ref: "Employee", 
+    },
+
+}, {timestamps: true});
+
+assetScheduledMaintenanceSchema.pre("save", function(next){
+    if(!this.maintenanceScheduledId){
+        this.maintenanceScheduledId = "MNTS"+this._id;
+    }
+    next();
+});
+
+export const AssetScheduledMaintenance = mongoose.model("AssetScheduledMaintenance", assetScheduledMaintenanceSchema);
