7ff54415759c1c631078f446f57e720e8ba63ca7
last-blade <meprashanttyagi2000@gmail.com>
2025-08-05 14:13:36 +0530

    remove conditional fetching

--- src/controllers/userControllers/hrManagementControllers/employeeControllers/getEmployees.controller.js
+++ src/controllers/userControllers/hrManagementControllers/employeeControllers/getEmployees.controller.js
@@ -2,7 +2,7 @@ import { apiResponse, asyncHandler, Employee } from "../../../allImports.js";
 
 const getEmployees = asyncHandler(async (request, response) => {
   const employees = await Employee.find({
-    employeeCreator: request.user.id,
+    // employeeCreator: request.user.id,
   }).populate("department", "department").select("-employeeCreator -refreshToken -password");
 
   return response
