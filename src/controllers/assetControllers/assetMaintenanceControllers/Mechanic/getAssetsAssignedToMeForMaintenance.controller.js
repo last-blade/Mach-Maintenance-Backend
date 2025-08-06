2d074b837b78bb60a9c03bcc04d7eecbf6fee9af
last-blade <meprashanttyagi2000@gmail.com>
2025-08-06 15:25:54 +0530

    In previous code not populating location, but now in this code populating location by applying aggregation pipeline

--- src/controllers/assetControllers/assetMaintenanceControllers/Mechanic/getAssetsAssignedToMeForMaintenance.controller.js
+++ src/controllers/assetControllers/assetMaintenanceControllers/Mechanic/getAssetsAssignedToMeForMaintenance.controller.js
@@ -1,9 +1,58 @@
+import mongoose from "mongoose";
 import { apiResponse, Asset, AssetMaintenance, asyncHandler } from "../../../allImports.js";
 
 const getAssetsAssignedToMeForMaintenance = asyncHandler(async (request, response) => {
-    const assetsAssignedToMeForMaintenance = await AssetMaintenance.find({
-        mechanic: request.user.id,
-    }).populate("assetId");
+    // const assetsAssignedToMeForMaintenance = await AssetMaintenance.find({
+    //     mechanic: request.user.id,
+    // }).populate("assetId");
+
+    const assetsAssignedToMeForMaintenance = await AssetMaintenance.aggregate([
+        {
+            $match: {
+                mechanic: new mongoose.Types.ObjectId(request.user.id),
+            }
+        },
+
+        {
+            $lookup: {
+                from: "assets",
+                let: {assetID: "$assetId"},
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: {
+                                $eq: ["$_id", "$$assetID"]
+                            }
+                        }
+                    },
+
+                    {
+                        $lookup: {
+                            from: "locations",
+                            localField: "assetLocation",
+                            foreignField: "_id",
+                            as: "location"
+                        }
+                    },
+
+                    {
+                        $unwind: {
+                            path: "$location",
+                            preserveNullAndEmptyArrays: true
+                        }
+                    }
+                ],
+                as: "assetId"
+            }
+        },
+
+        {
+            $unwind: {
+                path: "$assetId",
+                preserveNullAndEmptyArrays: true,
+            }
+        }
+    ])
 
     if(assetsAssignedToMeForMaintenance.length === 0){
         return response.status(200)
