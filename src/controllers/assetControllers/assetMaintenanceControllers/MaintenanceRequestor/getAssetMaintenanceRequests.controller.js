0b2356f4d2aea9e7694790fd18caabb622aaa532
last-blade <meprashanttyagi2000@gmail.com>
2025-08-06 17:34:51 +0530

    using aggregation pipeline to populate the location field in assetId

--- src/controllers/assetControllers/assetMaintenanceControllers/MaintenanceRequestor/getAssetMaintenanceRequests.controller.js
+++ src/controllers/assetControllers/assetMaintenanceControllers/MaintenanceRequestor/getAssetMaintenanceRequests.controller.js
@@ -14,10 +14,45 @@ const getAssetMaintenanceRequests = asyncHandler(async (request, response) => {
         model = "Employee"
     }
 
-    const assetMaintenanceRequests = await AssetMaintenanceRequest.find({
-        // assetMaintenanceRequestCreator: request.user.id,
-        // creatorModel: model,
-    }).populate("assetMaintenanceRequestCreator", "fullName email").populate("assetId");
+    // const assetMaintenanceRequests = await AssetMaintenanceRequest.find({
+    //     // assetMaintenanceRequestCreator: request.user.id,
+    //     // creatorModel: model,
+    // }).populate("assetMaintenanceRequestCreator", "fullName email").populate("assetId");
+
+    const assetMaintenanceRequests = await AssetMaintenanceRequest.aggregate([
+        {
+            $lookup: {
+                from: "assets",
+                let: {assetID: "$assetId"},
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: {
+                                $eq: ["$_id", "$$assetID"]
+                            }
+                        }
+                    },
+
+                    {
+                        $lookup: {
+                            from: "locations",
+                            localField: "assetLocation",
+                            foreignField: "_id",
+                            as: "location"
+                        }
+                    },
+
+                    {
+                        $unwind: {
+                            path: "$location",
+                            preserveNullAndEmptyArrays: true
+                        }
+                    }
+                ],
+                as: "assetId"
+            }
+        }
+    ])
 
     return response.status(200)
     .json(
