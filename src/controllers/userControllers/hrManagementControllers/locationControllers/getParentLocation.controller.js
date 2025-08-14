5b29930bcc46d9607ccb51fd9a003b7eff997adb
last-blade <meprashanttyagi2000@gmail.com>
2025-08-14 12:01:49 +0530

    remove isFactory filter from getParentLocation query

--- src/controllers/userControllers/hrManagementControllers/locationControllers/getParentLocation.controller.js
+++ src/controllers/userControllers/hrManagementControllers/locationControllers/getParentLocation.controller.js
@@ -3,7 +3,7 @@ import { apiResponse, asyncHandler, Location } from "../../../allImports.js";
 const getParentLocation = asyncHandler(async (request, response) => {
     const location = await Location.find({
         // locationCreator: request.user.id,
-        isFactory: true,
+        // isFactory: true,
     }).select("-locationCreator -__v");
 
     return response.status(200)
