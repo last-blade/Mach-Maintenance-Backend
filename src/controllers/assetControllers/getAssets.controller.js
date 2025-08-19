9529ecbfeeb9824b7a21dc008dbe87cd52929859
last-blade <meprashanttyagi2000@gmail.com>
2025-08-19 10:49:32 +0530

    refactor getAssets controller to comment out pagination logic

--- src/controllers/assetControllers/getAssets.controller.js
+++ src/controllers/assetControllers/getAssets.controller.js
@@ -2,18 +2,19 @@ import { apiResponse, Asset, asyncHandler } from "../allImports.js";
 
 const getAssets = asyncHandler(async (request, response) => {
 
-    const page = parseInt(request.query.page) || 10;
-    const limit = parseInt(request.query.limit) || 1;
-    const skip = (page - 1) * limit;
+    // const page = parseInt(request.query.page) || 10;
+    // const limit = parseInt(request.query.limit) || 1;
+    // const skip = (page - 1) * limit;
 
-    const totalAssets = await Asset.countDocuments();
+    // const totalAssets = await Asset.countDocuments();
 
     const assets = await Asset.find({
         // assetCreator: request.user.id,
     }).populate("assetLocation", "locationName locationCode")
     .populate("assetBrand", "assetBrand assetBrandDescription")
     .populate("assetCategory", "assetCategory depreciationPercentage")
-    .populate("assetSupplier", "assetSupplierName assetCompanyName").skip(skip).limit(limit).sort({createdAt: -1});
+    .populate("assetSupplier", "assetSupplierName assetCompanyName")
+    // .skip(skip).limit(limit).sort({createdAt: -1});
 
     return response.status(200)
     .json(
